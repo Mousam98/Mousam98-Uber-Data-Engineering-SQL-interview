1810e1f36aee138e3746aa38a6c99a6fd2d2f268
Mousam Dey <76545841+Mousam98@users.noreply.github.com>
2025-07-22 17:28:06 +0530

    Update query.py

--- QnA 4/query.py
+++ QnA 4/query.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'schema')))
-from spark import df
+from spark import df, spark
 
 df.createOrReplaceTempView('uber')
 
